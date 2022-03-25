2371e27d5c709b9a6ed68197e11dcf7069f5d777
Rayhan-dev <mohammodrayhan444@gmail.com>
2022-03-26 00:03:11 +0600

    bootstrap added and header completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
     </div>
   );
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-primary my-3'>Keyboard Maze</h1>
+            <h3>Choose 4 Keyboards</h3>
+        </div>
+    );
+};
+
+export default Header;
